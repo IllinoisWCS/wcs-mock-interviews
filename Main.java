5abf21afc62d61e1121c25da9ee3964913f25868
Melissa Chen <myc2@illinois.edu>
2020-08-24 14:48:40 -0500

    adding java template

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main {
+  // the function must return a value (cannot return void)
+  public static int function() {
+    // complete this function for the interview
+    // feel free to change the name of the function
+    // or its return type (to any non-void type)
+    // you should also add parameters for testing
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    // format: assert(EXPECTED == ACTUAL)
+
+    // put all test cases below this line
+    // copy and paste this line and replace with correct values or function names
+    assert 0 == function();
+
+    // put all test cases above this line
+    
+    // if the test passes, it will output "CORRECT"
+    // if the test fails, the function will throw an AssertionError
+    System.out.println("CORRECT");
+  }
+}
