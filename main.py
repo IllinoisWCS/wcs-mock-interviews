cf4abe0dac67f60ebc4e47ef7f8f136e8a1d7d07
Melissa Chen <myc2@illinois.edu>
2020-08-24 14:47:40 -0500

    adding python template

---
+++ main.py
@@ -0,0 +1,24 @@
+def function():
+  # complete this function for the interview
+  # feel free to change the name of the function
+  # you should also add parameters for testing
+
+  # the function should return the computed result 
+  return 0
+
+
+def testing():
+  # format: assert EXPECTED == ACTUAL
+
+  # put all test cases below this line
+  # copy and paste this line and replace with correct values or function names
+  assert 0 == function() 
+  
+  # put all test cases above this line
+
+  # if the test passes, it will output "CORRECT"
+  # if the test fails, an AssertionError will be thrown
+
+  print("CORRECT")
+
+testing()
