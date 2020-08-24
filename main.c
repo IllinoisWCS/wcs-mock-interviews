d99f812eaaeb8d088d1a6aee9bbb0989f171b50c
Melissa Chen <myc2@illinois.edu>
2020-08-24 17:07:28 -0500

    adding c template

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <assert.h>
+
+// the function must return a value (cannot return void)
+int function() {
+  // complete this function for the interview
+  // feel free to change the name of the function
+  // or its return type (to any non-void type)
+  // you should also add parameters for testing
+
+  // the function should return the computed result
+  return 0;
+}
+
+int main() {
+  // format: assert(EXPECTED == ACTUAL)
+
+  // put all test cases below this line
+  // copy and paste this line and replace with correct values or function names
+  assert(0 == function());
+
+  // put all test cases above this line
+  
+  // if the test passes, it will output "CORRECT"
+  // if the test fails, the function will abort and an error will be outputted
+  printf("CORRECT\n");
+
+  return 0;
+}
